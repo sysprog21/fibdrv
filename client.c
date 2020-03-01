002e64b2e8de24e0a5ea2e96cf8bb04a7b28c394
Jim Huang <jserv@biilabs.io>
2020-03-01 11:56:53 +0000

    Shorten user program

--- client.c
+++ client.c
@@ -9,27 +9,24 @@
 
 int main()
 {
-    int fd;
     long long sz;
 
     char buf[1];
     char write_buf[] = "testing writing";
-    int offset = 100;  // TODO: test something bigger than the limit
-    int i = 0;
+    int offset = 100; /* TODO: try test something bigger than the limit */
 
-    fd = open(FIB_DEV, O_RDWR);
-
+    int fd = open(FIB_DEV, O_RDWR);
     if (fd < 0) {
         perror("Failed to open character device");
         exit(1);
     }
 
-    for (i = 0; i <= offset; i++) {
+    for (int i = 0; i <= offset; i++) {
         sz = write(fd, write_buf, strlen(write_buf));
         printf("Writing to " FIB_DEV ", returned the sequence %lld\n", sz);
     }
 
-    for (i = 0; i <= offset; i++) {
+    for (int i = 0; i <= offset; i++) {
         lseek(fd, i, SEEK_SET);
         sz = read(fd, buf, 1);
         printf("Reading from " FIB_DEV
@@ -38,7 +35,7 @@ int main()
                i, sz);
     }
 
-    for (i = offset; i >= 0; i--) {
+    for (int i = offset; i >= 0; i--) {
         lseek(fd, i, SEEK_SET);
         sz = read(fd, buf, 1);
         printf("Reading from " FIB_DEV
