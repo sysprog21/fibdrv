488155300b157d6d9e6f7d846f7099b59bf5855d
Chun <roger20020122@gmail.com>
2023-03-13 14:12:01 +0800

    Correct bn_fib_sequence

--- fibdrv.c
+++ fibdrv.c
@@ -70,7 +70,7 @@ static void bn_fib_sequence(bn *dest, int k)
         bn_mult(a, a, a);
         bu_mult(b, b, b);
         bn_add(t2, a, b);
-        if (n & i) {
+        if (k & i) {
             bn_cpy(a, t2);
             bn_cpy(b, t1);
             bn_add(b, t2, b);
@@ -78,144 +78,144 @@ static void bn_fib_sequence(bn *dest, int k)
             bn_cpy(a, t1);
             bn_cpy(b, t2);
         }
-        bn_free(b);
-        bn_free(t1);
-        bn_free(t2);
-        return;
     }
+    bn_free(b);
+    bn_free(t1);
+    bn_free(t2);
+}
 
-    static int fib_open(struct inode * inode, struct file * file)
-    {
-        if (!mutex_trylock(&fib_mutex)) {
-            printk(KERN_ALERT "fibdrv is in use");
-            return -EBUSY;
-        }
-        return 0;
+static int fib_open(struct inode * inode, struct file * file)
+{
+    if (!mutex_trylock(&fib_mutex)) {
+        printk(KERN_ALERT "fibdrv is in use");
+        return -EBUSY;
     }
+    return 0;
+}
 
-    static int fib_release(struct inode * inode, struct file * file)
-    {
-        mutex_unlock(&fib_mutex);
-        return 0;
-    }
+static int fib_release(struct inode * inode, struct file * file)
+{
+    mutex_unlock(&fib_mutex);
+    return 0;
+}
 
-    static long long fib_time_proxy(long long k)
-    {
-        kt = ktime_get();
-        long long result = fib_sequence_string(k, buf);
-        kt = ktime_sub(ktime_get(), kt);
+static long long fib_time_proxy(long long k)
+{
+    kt = ktime_get();
+    long long result = fib_sequence(k);
+    kt = ktime_sub(ktime_get(), kt);
 
-        return result;
-    }
+    return result;
+}
 
-    static ssize_t fib_read(struct file * file, char *buf, size_t size,
+static ssize_t fib_read(struct file * file, char *buf, size_t size,
+                        loff_t *offset)
+{
+    return (ssize_t) fib_time_proxy(*offset);
+}
+
+static ssize_t fib_write(struct file * file, const char *buf, size_t size,
                             loff_t *offset)
-    {
-        return (ssize_t) fib_time_proxy(*offset);
+{
+    return ktime_to_ns(kt);
+}
+
+static loff_t fib_device_lseek(struct file * file, loff_t offset, int orig)
+{
+    loff_t new_pos = 0;
+    switch (orig) {
+    case 0: /* SEEK_SET: */
+        new_pos = offset;
+        break;
+    case 1: /* SEEK_CUR: */
+        new_pos = file->f_pos + offset;
+        break;
+    case 2: /* SEEK_END: */
+        new_pos = MAX_LENGTH - offset;
+        break;
     }
 
-    static ssize_t fib_write(struct file * file, const char *buf, size_t size,
-                             loff_t *offset)
-    {
-        return ktime_to_ns(kt);
-    }
+    if (new_pos > MAX_LENGTH)
+        new_pos = MAX_LENGTH;  // max case
+    if (new_pos < 0)
+        new_pos = 0;        // min case
+    file->f_pos = new_pos;  // This is what we'll use now
+    return new_pos;
+}
 
-    static loff_t fib_device_lseek(struct file * file, loff_t offset, int orig)
-    {
-        loff_t new_pos = 0;
-        switch (orig) {
-        case 0: /* SEEK_SET: */
-            new_pos = offset;
-            break;
-        case 1: /* SEEK_CUR: */
-            new_pos = file->f_pos + offset;
-            break;
-        case 2: /* SEEK_END: */
-            new_pos = MAX_LENGTH - offset;
-            break;
-        }
+const struct file_operations fib_fops = {
+    .owner = THIS_MODULE,
+    .read = fib_read,
+    .write = fib_write,
+    .open = fib_open,
+    .release = fib_release,
+    .llseek = fib_device_lseek,
+};
 
-        if (new_pos > MAX_LENGTH)
-            new_pos = MAX_LENGTH;  // max case
-        if (new_pos < 0)
-            new_pos = 0;        // min case
-        file->f_pos = new_pos;  // This is what we'll use now
-        return new_pos;
-    }
+static int __init init_fib_dev(void)
+{
+    int rc = 0;
 
-    const struct file_operations fib_fops = {
-        .owner = THIS_MODULE,
-        .read = fib_read,
-        .write = fib_write,
-        .open = fib_open,
-        .release = fib_release,
-        .llseek = fib_device_lseek,
-    };
+    mutex_init(&fib_mutex);
 
-    static int __init init_fib_dev(void)
-    {
-        int rc = 0;
+    // Let's register the device
+    // This will dynamically allocate the major number
+    rc = alloc_chrdev_region(&fib_dev, 0, 1, DEV_FIBONACCI_NAME);
 
-        mutex_init(&fib_mutex);
-
-        // Let's register the device
-        // This will dynamically allocate the major number
-        rc = alloc_chrdev_region(&fib_dev, 0, 1, DEV_FIBONACCI_NAME);
-
-        if (rc < 0) {
-            printk(KERN_ALERT
-                   "Failed to register the fibonacci char device. rc = %i",
-                   rc);
-            return rc;
-        }
-
-        fib_cdev = cdev_alloc();
-        if (fib_cdev == NULL) {
-            printk(KERN_ALERT "Failed to alloc cdev");
-            rc = -1;
-            goto failed_cdev;
-        }
-        fib_cdev->ops = &fib_fops;
-        rc = cdev_add(fib_cdev, fib_dev, 1);
-
-        if (rc < 0) {
-            printk(KERN_ALERT "Failed to add cdev");
-            rc = -2;
-            goto failed_cdev;
-        }
-
-        fib_class = class_create(THIS_MODULE, DEV_FIBONACCI_NAME);
-
-        if (!fib_class) {
-            printk(KERN_ALERT "Failed to create device class");
-            rc = -3;
-            goto failed_class_create;
-        }
-
-        if (!device_create(fib_class, NULL, fib_dev, NULL,
-                           DEV_FIBONACCI_NAME)) {
-            printk(KERN_ALERT "Failed to create device");
-            rc = -4;
-            goto failed_device_create;
-        }
-        return rc;
-    failed_device_create:
-        class_destroy(fib_class);
-    failed_class_create:
-        cdev_del(fib_cdev);
-    failed_cdev:
-        unregister_chrdev_region(fib_dev, 1);
+    if (rc < 0) {
+        printk(KERN_ALERT
+                "Failed to register the fibonacci char device. rc = %i",
+                rc);
         return rc;
     }
 
-    static void __exit exit_fib_dev(void)
-    {
-        mutex_destroy(&fib_mutex);
-        device_destroy(fib_class, fib_dev);
-        class_destroy(fib_class);
-        cdev_del(fib_cdev);
-        unregister_chrdev_region(fib_dev, 1);
+    fib_cdev = cdev_alloc();
+    if (fib_cdev == NULL) {
+        printk(KERN_ALERT "Failed to alloc cdev");
+        rc = -1;
+        goto failed_cdev;
+    }
+    fib_cdev->ops = &fib_fops;
+    rc = cdev_add(fib_cdev, fib_dev, 1);
+
+    if (rc < 0) {
+        printk(KERN_ALERT "Failed to add cdev");
+        rc = -2;
+        goto failed_cdev;
     }
 
-    module_init(init_fib_dev);
-    module_exit(exit_fib_dev);
+    fib_class = class_create(THIS_MODULE, DEV_FIBONACCI_NAME);
+
+    if (!fib_class) {
+        printk(KERN_ALERT "Failed to create device class");
+        rc = -3;
+        goto failed_class_create;
+    }
+
+    if (!device_create(fib_class, NULL, fib_dev, NULL,
+                        DEV_FIBONACCI_NAME)) {
+        printk(KERN_ALERT "Failed to create device");
+        rc = -4;
+        goto failed_device_create;
+    }
+    return rc;
+failed_device_create:
+    class_destroy(fib_class);
+failed_class_create:
+    cdev_del(fib_cdev);
+failed_cdev:
+    unregister_chrdev_region(fib_dev, 1);
+    return rc;
+}
+
+static void __exit exit_fib_dev(void)
+{
+    mutex_destroy(&fib_mutex);
+    device_destroy(fib_class, fib_dev);
+    class_destroy(fib_class);
+    cdev_del(fib_cdev);
+    unregister_chrdev_region(fib_dev, 1);
+}
+
+module_init(init_fib_dev);
+module_exit(exit_fib_dev);
